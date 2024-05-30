f8ced84e27319c673cec76eb3d00cac0aa3763dc
kglidiya <betysmith@yandex.ru>
2024-05-30 12:38:09 +0300

    add icon arrow

---
+++ frontend/src/ui/icons/arrow-down/ArrowDown.tsx
@@ -0,0 +1,30 @@
+export default function ArrowDown() {
+  return (
+    <svg height={52} width={52} fill='none' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='m22.0002 11.9999c0 5.515-4.486 10-10 10s-10-4.485-10-10c0-5.51399 4.486-9.99999 10-9.99999s10 4.486 10 9.99999'
+        fill='white'
+        opacity='1'
+      ></path>
+      <path
+        d='m16.2209 10.5575c0 .191-.073.383-.219.529l-3.47 3.487c-.141.141-.332.22-.532.22-.199 0-.39-.079-.531-.22l-3.472-3.487c-.292-.293-.292-.767.002-1.06.294-.29199.769-.29099 1.061.002l2.94 2.953 2.94-2.953c.292-.29299.766-.29399 1.06-.002.148.146.221.339.221.531'
+        fill='#ddd6c7'
+      ></path>
+    </svg>
+
+    // <svg height={32} width={32} viewBox='-2.64 -2.64 29.28 29.28' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    //   <g id='SVGRepo_bgCarrier' strokeWidth='0' />
+    //   <g id='SVGRepo_tracerCarrier' strokeLinecap='round' strokeLinejoin='round' />
+    //   <g id='SVGRepo_iconCarrier'>
+    //     {" "}
+    //     <path
+    //       d='M12 6V18M12 18L7 13M12 18L17 13'
+    //       stroke='#ddd6c7'
+    //       strokeWidth='4'
+    //       strokeLinecap='round'
+    //       strokeLinejoin='round'
+    //     />{" "}
+    //   </g>
+    // </svg>
+  );
+}
