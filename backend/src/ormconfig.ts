c7f1b88237184f4bdb85d196f714df33d579d850
kglidiya <betysmith@yandex.ru>
2024-06-07 21:20:01 +0300

    orm config

--- backend/src/ormconfig.ts
+++ backend/src/ormconfig.ts
@@ -11,9 +11,13 @@ const config: ConnectionOptions = {
   database: process.env.POSTGRES_DB,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
-  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  // migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  // cli: {
+  //   migrationsDir: 'src/migrations',
+  // },
   cli: {
-    migrationsDir: 'src/migrations',
+    migrationsDir: 'dist/migrations',
   },
   // logging: true,
 };
